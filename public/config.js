804a3ea7ea6172b0876545f4df75ad9dfae238ea
宝宝宝 <wangdabao@js.cool>
2024-11-25 11:48:10 +0800

    update config

--- public/config.js
+++ public/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2743301-954072b278d42d76d7725178'
   ],
 
   // 日志天数
@@ -20,17 +17,17 @@ window.Config = {
 
   // 导航栏菜单
   Navi: [
-    {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
-    },
+    // {
+    //   text: '周额',
+    //   url: 'https://status.940703.xyz'
+    // },
+    // {
+    //   text: 'GitHub',
+    //   url: 'https://github.com/yb/uptime-status'
+    // },
+    // {
+    //   text: 'Blog',
+    //   url: 'https://abo.xyz/'
+    // },
   ],
 };
